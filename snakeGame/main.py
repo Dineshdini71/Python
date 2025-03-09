c59858da11e81076ee5c384260622d75179264e8
Dineshdini71 <dinidinesh71@gmail.com>
2025-03-09 09:46:11 +0530

    Snake Game

---
+++ snakeGame/main.py
@@ -0,0 +1,43 @@
+from turtle import Turtle, Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+segment = []
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move_snake()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
