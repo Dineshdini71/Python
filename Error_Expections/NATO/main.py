023fc6fa064c83e6446d77d43569370ece1c3d95
Dineshdini71 <dinidinesh71@gmail.com>
2025-07-30 10:28:55 +0530

    Execptions Handling

---
+++ Error_Expections/NATO/main.py
@@ -0,0 +1,23 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+from dbm import error
+
+import pandas as pd
+
+data = pd.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError as e:
+        print("Sorry, Print only a alphabetic Numbers")
+        generate_phonetic()
+    else:
+        print(output_list)
+generate_phonetic()
+
+
+
---
+++ Error_Expections/Practice/main.py
@@ -0,0 +1,96 @@
+# FileNotFoundError
+# with open('a_file.txt', 'r') as file:
+#     print(file)
+from fileinput import close
+from fnmatch import fnmatch
+from tkinter import EXCEPTION
+
+# KeyError
+# dict = {'key': 'value'}
+# print(dict["dinesh"])
+
+# IndexError
+# dup = ['Apple','Ball', 'cat']
+# print(dup[3])
+
+# TypeError
+# a = 23 + '5'
+# print(a)
+
+# ----------------------- EXCEPTIONS -----------------------
+
+# 1. try
+# 2. except
+# 3. else
+# 4. finally
+
+# -----------------------------------------------------------
+
+# try:
+#     file = open('b_file.txt')
+#     b_dict = {'key': 'dinesh'}
+#     print(b_dict['key'])
+# except FileNotFoundError:
+#     file = open('b_file.txt', 'w')
+#     file.write('I am writing in except')
+# except KeyError as e:
+#     print(f"This key {e} id doesn't exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error which I made UP")
+
+# -----------------------------------------------------------
+
+# ------------------- RAISE OWN EXCEPTION -------------------
+
+height = float(input("Height: " ))
+weight = int(input("Weight: " ))
+
+if height > 3:
+    raise ValueError("Human height is not greater than 3 meters...")
+
+bmi = weight / height ** 2
+
+print(bmi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
